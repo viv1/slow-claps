45eb5fca1b7c952f0b0b899464c63ec133b63964
Vivek Sahu <viveksahu.01@gmail.com>
2020-06-05 21:24:49 +0530

    add intent handlers for cancel, exit, help with localization support

---
+++ index.js
@@ -0,0 +1,141 @@
+//
+// Alexa Fact Skill - Play Slow Claps
+//
+
+// sets up dependencies
+const Alexa = require('ask-sdk-core');
+const i18n = require('i18next');
+
+const HelpHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('HELP_MESSAGE'))
+      .reprompt(requestAttributes.t('HELP_REPROMPT'))
+      .getResponse();
+  },
+};
+
+const FallbackHandler = {
+  // The FallbackIntent can only be sent in those locales which support it,
+  // so this handler will always be skipped in locales where it is not supported.
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'AMAZON.FallbackIntent';
+  },
+  handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('FALLBACK_MESSAGE'))
+      .reprompt(requestAttributes.t('FALLBACK_REPROMPT'))
+      .getResponse();
+  },
+};
+
+const ExitHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && (request.intent.name === 'AMAZON.CancelIntent'
+        || request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('STOP_MESSAGE'))
+      .reprompt(requestAttributes.t('STOP_MESSAGE'))
+      .getResponse();
+  },
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+    console.log(`Error stack: ${error.stack}`);
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('ERROR_MESSAGE'))
+      .reprompt(requestAttributes.t('ERROR_MESSAGE'))
+      .getResponse();
+  },
+};
+
+const LocalizationInterceptor = {
+  process(handlerInput) {
+    // Gets the locale from the request and initializes i18next.
+    const localizationClient = i18n.init({
+      lng: handlerInput.requestEnvelope.request.locale,
+      resources: languageStrings,
+      returnObjects: true
+    });
+    // Creates a localize function to support arguments.
+    localizationClient.localize = function localize() {
+      // gets arguments through and passes them to
+      // i18next using sprintf to replace string placeholders
+      // with arguments.
+      const args = arguments;
+      const value = i18n.t(...args);
+      // If an array is used then a random value is selected
+      if (Array.isArray(value)) {
+        return value[Math.floor(Math.random() * value.length)];
+      }
+      return value;
+    };
+    // this gets the request attributes and save the localize function inside
+    // it to be used in a handler by calling requestAttributes.t(STRING_ID, [args...])
+    const attributes = handlerInput.attributesManager.getRequestAttributes();
+    attributes.t = function translate(...args) {
+      return localizationClient.localize(...args);
+    }
+  }
+};
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    HelpHandler,
+    ExitHandler,
+    FallbackHandler,
+    SessionEndedRequestHandler,
+  )
+  .addRequestInterceptors(LocalizationInterceptor)
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
+
+const enData = {
+  translation: {
+    SKILL_NAME: 'Slow Claps',
+    HELP_MESSAGE: 'Ask me to start slow claps',
+    HELP_REPROMPT: 'Ask me to start slow claps',
+    FALLBACK_MESSAGE: 'Ask me to start slow claps',
+    FALLBACK_REPROMPT: 'Ask me to start slow claps',
+    ERROR_MESSAGE: 'Sorry, an error occurred.',
+    STOP_MESSAGE: 'Goodbye!',
+  },
+};
+
+// constructs i18n and l10n data structure
+const languageStrings = {
+  'en': enData,
+};
